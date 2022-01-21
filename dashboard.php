948800d7e17ba40f5c4e093583a5b94cf3921a34
aclaputra <muhamadacla22@gmail.com>
2022-01-21 09:16:00 +0700

    added dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <nav>
+      <img src="./meta-logo.png" alt="logo-meta" width="100" height="20">
+    </nav>
+</body>
+</html>

76b7f91f5c2cc2a10d71b79a0a1f99400253b658
aclaputra <muhamadacla22@gmail.com>
2022-01-22 00:42:02 +0700

    include database

--- dashboard.php
+++ dashboard.php
@@ -30,21 +30,22 @@
 
     <form class="m-auto mt-4" style="width: 80%;" action="add_siswa.php" method="post" name="form1">
         <div class="mb-4">
-        <label for="exampleInputEmail1" class="form-label">Nama:</label>
+          <h1>New Metaverse</h1>
+        <label for="exampleInputEmail1" class="form-label">Title:</label>
         
-        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Calon Mahasiswa" name="nama">
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your Metaverse Title" name="title">
         </div>
         
         <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Nomor HP:</label>
+        <label for="exampleInputEmail1" class="form-label">Image:</label>
         
-        <input type="tel" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="No Handphone" name="nohp">
+        <input type="tel" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your Image" name="image">
         </div>
         
         <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email:</label>
+        <label for="exampleInputEmail1" class="form-label">Location:</label>
         
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email Rinfo" name="email">
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Redirect to.." name="location">
         </div>
         
         <input class="btn btn-primary m-2 p-2 px-4 shadow-lg rounded-pill m-auto" type="submit" name="submit" value="Submit Here">
@@ -52,14 +53,15 @@
 
     <?php
         if(isset($_POST['submit'])){
-        $nama = $_POST['nama'];
-        $no = $_POST['nohp'];
-        $email = $_POST['email'];
+        $title = $_POST['title'];
+        $image = $_POST['image'];
+        $location = $_POST['location'];
             
-        $query_pg = "INSERT INTO calon_mahasiswa(cln_nama,cln_no,cln_email) VALUES('$nama','$no','$email')";
+        include("config.php");
+
+        $result = mysqli_query($mysqli, "INSERT INTO heroes_tb(name,type_id,photo) VALUES('$title',$image,'$location')");
             
-            
-        $result = pg_query($query_pg);
+        $result = pg_query($query_sql);
         echo "<p class='text-center'>Siswa Added Successfully.</p> <a href='../index.php' class='text-center'>View Siswa</a>";
         
         }

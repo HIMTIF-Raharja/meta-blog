8478a992cbd2d407eff9f50557dd4b0693d14129
aclaputra <muhamadacla22@gmail.com>
2022-01-22 00:56:45 +0700

    fix dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -33,19 +33,19 @@
           <h1>New Metaverse</h1>
         <label for="exampleInputEmail1" class="form-label">Title:</label>
         
-        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your Metaverse Title" name="title">
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your Metaverse Title (ex: Fantasy)" name="title">
         </div>
         
         <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Image:</label>
         
-        <input type="tel" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your Image" name="image">
+        <input type="tel" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your Metaverse Image (ex: tmc1)" name="image">
         </div>
         
         <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Location:</label>
         
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Redirect to.." name="location">
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Redirect to.. (ex: metaverse/fantasy)" name="location">
         </div>
         
         <input class="btn btn-primary m-2 p-2 px-4 shadow-lg rounded-pill m-auto" type="submit" name="submit" value="Submit Here">
@@ -60,9 +60,12 @@
           include("config.php");
 
           $result = mysqli_query($mysqli, "INSERT INTO heroes_tb(name,type_id,photo) VALUES('$title',$image,'$location')");
-              
-          $result = pg_query($query_sql);
-          echo "<p class='text-center'>New Metaverse Added Successfully.</p> <a href='../index.php' class='text-center'>View Siswa</a>";
+
+          if($result){
+            echo "<script>alert('Successfully Added!')</script>";
+            echo "<script>window.open('dashboard.php','_self')</script>";
+          }
+          // echo "<p class='text-center'>New Metaverse Added Successfully.</p> <a href='../index.php' class='text-center'>View Siswa</a>";
         }
     ?>
 </body>

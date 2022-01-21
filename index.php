6192202bbd8893dcea85981fd05488d31ad114ab
aclaputra <muhamadacla22@gmail.com>
2022-01-21 23:46:22 +0700

    modified index.php

--- index.php
+++ index.php
@@ -6,12 +6,30 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome to meta - Facebook</title>
     <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   </head>
   <body>
-    <nav>
+    <!-- <nav>
       <img src="./meta-logo.png" alt="logo-meta" width="100" height="20">
+    </nav> -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="./meta-logo.png" alt="logo-meta" width="100" height="20"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="dashboard.php">Dashboard</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
-
     <header>
       <div class="header__container">
         <img src="./ip13pro_mockup.png" alt="mockup" class="left__header__img left__animation" height="900">
